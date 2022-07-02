ac75ba09bdf874959aae5627e7404e8a2d9c62b1
Wandji69 <collinschuwa@gmail.com>
2022-07-02 02:25:20 +0100

    the calculate algorithm using functions and import

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,7 +2,7 @@
 import calculator_1 as cal
 a = 10
 b = 5
-print("{} + {} = {}".format(a, b, cal.sum(a, b)))
+print("{} + {} = {}".format(a, b, cal.add(a, b)))
 print("{} - {} = {}".format(a, b, cal.sub(a, b)))
 print("{} * {} = {}".format(a, b, cal.mul(a, b)))
 print("{} / {} = {}".format(a, b, cal.div(a, b)))
